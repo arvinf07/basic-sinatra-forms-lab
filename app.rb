eac9095c97e8858ba6a7d6b70d46dc1b952cf4fb
Arvin Fernandez <arvinf07@gmail.com>
2020-11-30 16:23:01 -0500

    commit tree fb4405a59efdea8932905229111efad5d9d705cc

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end  
 
+  post '/team' do
+    params.each do |key, value|
+      self.class.attr_accessor key.to_sym
+      self.send(("#{key}="), value)
+    end  
+    erb :team
+  end  
+  
 end
